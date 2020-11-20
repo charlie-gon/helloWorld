091a6e52144aaa4859535783630f82099f340699
admin <admin@YD03-02>
2020-11-20 13:41:02 +0900

    22

--- variable/src/array/Practice5_9_201120.java
+++ variable/src/array/Practice5_9_201120.java
@@ -13,12 +13,12 @@ public class Practice5_9_201120 {
 		Scanner scanner = new Scanner(System.in);
 
 		while (run) {
-			System.out.println("----------------------------------");
+			System.out.println("---------------------------------------------");
 			System.out.println("1.학생수 | 2.점수입력 | 3.점수리스트 | 4.분석 | 5.종료");
-			System.out.println("----------------------------------");
+			System.out.println("---------------------------------------------");
 			System.out.print("선택> ");
 
-			int selectNo = scanner.nextInt();
+			int selectNo = scanner.nextInt(); // 앞서 선언했던 변수 scanner를 정수 타입으로 리턴
 
 			if (selectNo == 1) {
 				System.out.print("학생수> ");
@@ -30,17 +30,30 @@ public class Practice5_9_201120 {
 					scores[i] = scanner.nextInt();
 				}
 			} else if (selectNo == 3) {
-				for(int i = 0; i<scores.length; i++) {
+				for (int i = 0; i < scores.length; i++) {
 					System.out.println(scores[i]);
+				}
 			} else if (selectNo == 4) {
-				int avg = 0;
 				int sum = 0;
-				
+				int maxValue = 0;
+				double avg = 0;
+				for (int i = 0; i < scores.length; i++) {
+					if (scores[i] > maxValue) {
+						maxValue = scores[i];
+					}
+					sum += scores[i];
+				}
+				avg = (double) sum / scores.length;
+
+				System.out.println("최고 점수: " + maxValue);
+				System.out.println("평균 점수: " + avg);
+
+			} else if (selectNo == 5) {
+				run = false;
 			}
 
 		}
+		System.out.println("프로그램 종료");
 
 	}
-
-}
 }
